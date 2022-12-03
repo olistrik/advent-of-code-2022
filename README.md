ae974dd51c4523c6d2611952e7b59df6bb802e25
Kranex <oliverstrik@gmail.com>
2022-12-03 17:00:13 +0100

    day 1

---
+++ README.md
@@ -0,0 +1,9 @@
+# Rules
+
+All functions must be in scss.
+
+# Usage
+
+```
+./compile.js src/1a.scss input/1.in
+```
---
+++ compile.js
@@ -0,0 +1,19 @@
+#!/usr/bin/env node
+const sass = require("sass");
+const fs = require("fs");
+
+const file = process.argv[2];
+const input = process.argv[3];
+
+var lines = fs.readFileSync(input, { encoding: "utf8" });
+lines = lines.split("\n").slice(0, -1);
+
+const result = sass.compile(file, {
+  functions: {
+    "input()": function () {
+      return new sass.SassList(lines.map((val) => new sass.SassString(val)));
+    },
+  },
+});
+
+console.log(result.css);
