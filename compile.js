850b21735446e9ffa7c0aba5d50a5d3b9979ffa2
Kranex <oliverstrik@gmail.com>
2022-12-06 21:37:03 +0100

    day 6

--- compile.js
+++ compile.js
@@ -5,17 +5,43 @@ const fs = require("fs");
 const file = process.argv[2];
 const input = process.argv[3];
 
-var lines = fs.readFileSync(input, { encoding: "utf8" });
-lines = lines.split("\n").slice(0, -1);
+const fd = fs.openSync(input, "r");
+var idx = 0;
+
+function readChar() {
+  let buffer = Buffer.alloc(1);
+  fs.readSync(fd, buffer, 0, 1);
+  return buffer.toString("utf8");
+}
+
+function readLine() {
+  var line = undefined;
+
+  var char = readChar();
+  while (char != "\0" && char !== "\n") {
+    line = (line ?? "") + char;
+
+    char = readChar();
+  }
+
+  return line;
+}
+
+function wrapSassString(fn) {
+  return function () {
+    return sass.SassString(fn());
+  };
+}
 
 const result = sass.compile(file, {
   functions: {
     "input()": function () {
+      var lines = fs.readFileSync(input, { encoding: "utf8" });
+      lines = lines.split("\n").slice(0, -1);
       return new sass.SassList(lines.map((val) => new sass.SassString(val)));
     },
-    "read-line()": function () {
-      return new sass.SassString(lines.shift());
-    },
+    "read-line()": wrapSassString(readLine),
+    "read-char()": wrapSassString(readChar),
   },
 });
 
